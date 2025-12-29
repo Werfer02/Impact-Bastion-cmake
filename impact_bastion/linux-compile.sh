ccbda2356a0c2191fd4780264a63418fb3b3ff1a
jacek <jacekjanusiewicz02@gmail.com>
2025-12-29 19:59:05 +0000

    switch project over to use cmake

--- impact_bastion/linux-compile.sh
+++
@@ -1,2 +0,0 @@
-g++ main.cpp -o impact-bastion -std=c++17   -I../SFML-3.0.2/include   -L../SFML-3.0.2/lib   -Wl,-rpath,../SFML-3.0.2/lib   -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
-
